4d356cd121b916a2fd55837764196dbbe63858dd
Sophie Mann <sklm@live.co.uk>
2020-07-10 12:34:22 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Rogue from "./Rogue";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Title</h1>
+    <p>content</p>
+    <Rogue></Rogue>
+  </div>
+);
 
 export default App;
---
+++ src/Rogue.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rogue = () => (
+  <canvas
+    width="256"
+    height="256"
+    style={{ border: "1px solid black" }}
+  ></canvas>
+);
+
+export default Rogue;
