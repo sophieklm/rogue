86c5c457c74a963c43f7c419b867440fb0bcdcb0
Sophie Mann <sklm@live.co.uk>
2020-07-10 13:01:22 +0100

    Add input manager to handle key press

--- src/Rogue.js
+++ src/Rogue.js
@@ -1,14 +1,32 @@
 import React, { useRef, useEffect } from "react";
+import InputManager from "./InputManager";
 
 const Rogue = ({ width, height, size }) => {
   const canvasRef = useRef();
+
+  let inputManager = new InputManager();
+
+  const handleInput = (action, data) => {
+    console.log(`handle input: ${action}:${JSON.stringify(data)}`);
+  };
+
   useEffect(() => {
-    console.log("Draw to canvas");
+    console.log("Bind input");
+    inputManager.bindKeys();
+    inputManager.subscribe(handleInput);
+    return () => {
+      inputManager.unbindKeys();
+      inputManager.unsubscribe(handleInput);
+    };
+  });
+
+  useEffect(() => {
     const ctx = canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, width * size, height * size);
     ctx.fillStyle = "#000";
     ctx.fillRect(12, 22, 16, 16);
   });
+
   return (
     <canvas
       ref={canvasRef}
