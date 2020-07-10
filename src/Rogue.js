f1b3aa520f3acbbac2fd2cdb804fcf04a122a11f
Sophie Mann <sklm@live.co.uk>
2020-07-10 12:42:48 +0100

    Add hooks to  set and clear canvas

--- src/Rogue.js
+++ src/Rogue.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-const Rogue = ({ width, height, size }) => (
-  <canvas
-    width={width * size}
-    height={height * size}
-    style={{ border: "1px solid black" }}
-  ></canvas>
-);
+const Rogue = ({ width, height, size }) => {
+  const canvasRef = useRef();
+  useEffect(() => {
+    console.log("Draw to canvas");
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, width * size, height * size);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(12, 22, 16, 16);
+  });
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width * size}
+      height={height * size}
+      style={{ border: "1px solid black" }}
+    ></canvas>
+  );
+};
 
 export default Rogue;
